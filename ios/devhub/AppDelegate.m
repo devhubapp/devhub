93159fb82cea81f7654078822070eb2ad40e17ea
Bruno Lemos <brunohplemos@gmail.com>
2017-01-25 00:46:55 -0200

    Fixed white screen on ios splash screen

--- ios/devhub/AppDelegate.m
+++ ios/devhub/AppDelegate.m
@@ -34,7 +34,9 @@
                                                       moduleName:@"devhub"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  
+  // dark blue #141c26
+  rootView.backgroundColor = [UIColor colorWithRed:0.08 green:0.11 blue:0.15 alpha:1.0];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
