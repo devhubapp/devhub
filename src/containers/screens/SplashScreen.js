a34959f0676bce25579b2eea1f21abe7ff1f5646
Bruno Lemos <brunohplemos@gmail.com>
2017-03-05 03:12:08 -0300

    Added a Reset App Data button on SplashScreen in case it gets stuck somehow -- just a precaution

--- src/containers/screens/SplashScreen.js
+++ src/containers/screens/SplashScreen.js
@@ -1,23 +1,49 @@
 // @flow
 
 import React from 'react';
-import { ActivityIndicator, Platform, StatusBar } from 'react-native';
 import styled from 'styled-components/native';
+import { ActivityIndicator, Platform, StatusBar } from 'react-native';
+import { connect } from 'react-redux';
+
+import { resetAppDataRequest } from '../../actions';
 
 import Screen from '../../components/Screen';
-import { splashScreenBackgroundColor } from '../../styles/variables';
+import { contentPadding, splashScreenBackgroundColor } from '../../styles/variables';
 import { base00 } from '../../styles/themes/dark';
 
-const SplashScreen = styled(Screen)`
+const Root = styled(Screen)`
   flex: 1;
   align-items: center;
   justify-content: center;
 `;
 
-export default () => (
-  <SplashScreen backgroundColor={splashScreenBackgroundColor}>
+const ResetButton = styled.TouchableOpacity`
+  position: absolute;
+  left: ${contentPadding}px;
+  right: ${contentPadding}px;
+  bottom: ${contentPadding}px;
+  opacity: 0.3;
+`;
+
+const ResetButtonText = styled.Text`
+  text-align: center;
+  color: ${({ theme }) => theme.base00 || '#000000'};
+`;
+
+const mapDispatchToProps = ({ resetAppData: resetAppDataRequest });
+
+type Props = { resetAppData: Function };
+const SplashScreen = ({ resetAppData }: Props) => (
+  <Root backgroundColor={splashScreenBackgroundColor}>
     {(Platform.OS === 'ios' || Platform.OS === 'android') &&
       <StatusBar hidden />}
+
     <ActivityIndicator color={base00} />
-  </SplashScreen>
+
+    <ResetButton onPress={() => resetAppData()} activeOpacity={0.1} focusedOpacity={1}>
+      <ResetButtonText>Reset app data</ResetButtonText>
+    </ResetButton>
+  </Root>
 );
+
+export default connect(null, mapDispatchToProps)(SplashScreen);
