76bc8d05931bc027b9a152b10e2dbb6712ab9bac
Bruno Lemos <brunohplemos@gmail.com>
2017-05-02 23:59:56 -0300

    Fixed bugsnag crash on web

--- src/libs/bugsnag/index.web.js
+++ src/libs/bugsnag/index.web.js
@@ -5,6 +5,15 @@ import Bugsnag from 'bugsnag-js'
 
 export default apiKey => {
   Bugsnag.apiKey = apiKey
+
+  Bugsnag.clearUser = () => (Bugsnag.user = null)
+
+  Bugsnag.setUser = (id, name, email, other = {}) =>
+    (Bugsnag.user = { id, name, email, ...other })
+
+  Bugsnag.notify = Bugsnag.notifyException
+  delete Bugsnag.notifyException
+
   return Bugsnag
 }
 
