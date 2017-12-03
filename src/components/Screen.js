96ac0349c934cb5339f785d04782a9b332443d36
Bruno Lemos <brunohplemos@gmail.com>
2017-12-03 21:04:53 -0200

    Support iPhone X
    That was easy

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,13 +1,14 @@
 /* eslint-env browser */
 
 import styled from 'styled-components/native'
+import { SafeAreaView } from 'react-native'
 
 import Platform from '../libs/platform'
 
 const getBackgroundColorFromProps = ({ backgroundColor, theme }) =>
   backgroundColor || (theme || {}).base00
 
-export default styled.View`
+export default styled(SafeAreaView)`
   flex: 1;
   padding-top: ${Platform.realOS === 'ios' && Platform.isStandalone ? 22 : 0}px;
   background-color: ${getBackgroundColorFromProps};
