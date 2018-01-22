4bb2cde237cb5cab797e57e368e85a266c41dc47
Bruno Lemos <brunohplemos@gmail.com>
2018-01-22 02:19:28 -0200

    Upgrade react-native to 0.52

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,3 +1,5 @@
+const blacklist = require('metro/src/blacklist') // eslint-disable-line
+
 module.exports = {
   getTransformModulePath() {
     return require.resolve('react-native-typescript-transformer')
@@ -5,5 +7,11 @@ module.exports = {
 
   getSourceExts() {
     return ['ts', 'tsx']
+  },
+
+  // Workaround for RN 0.52 issue with react-native-vector-icons
+  // See: https://github.com/oblador/react-native-vector-icons/issues/626#issuecomment-357469857
+  getBlacklistRE() {
+    return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/])
   },
 }
