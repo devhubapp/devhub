c4e3751ab3bd47b5215a02a177eb729681d62e1f
Bruno Lemos <brunohplemos@gmail.com>
2018-03-25 15:08:22 -0300

    Fix API data not updating + Fix order

--- src/containers/EventCardsContainer.tsx
+++ src/containers/EventCardsContainer.tsx
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { PureComponent } from 'react'
 import { Alert } from 'react-native'
 
@@ -18,10 +19,12 @@ export default class EventCardsContainer extends PureComponent<IProps, IState> {
   async componentDidMount() {
     try {
       const response = await fetch(
-        'https://api.github.com/users/brunolemos/received_events?access_token=fae0e8d5d55b71afb4c59d6abb89fce457c48160',
+        `https://api.github.com/users/brunolemos/received_events?access_token=fae0e8d5d55b71afb4c59d6abb89fce457c48160&timestamp=${Date.now()}`,
       )
       const events = await response.json()
-      if (Array.isArray(events)) this.setState({ events })
+      if (Array.isArray(events)) {
+        this.setState({ events: _.orderBy(events, ['updated_at'], ['desc']) })
+      }
     } catch (error) {
       console.error(error)
       Alert.alert('Failed to load events', `${error}`)
--- src/containers/NotificationCardsContainer.tsx
+++ src/containers/NotificationCardsContainer.tsx
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { PureComponent } from 'react'
 import { Alert } from 'react-native'
 
@@ -21,10 +22,14 @@ export default class NotificationCardsContainer extends PureComponent<
   async componentDidMount() {
     try {
       const response = await fetch(
-        'https://api.github.com/notifications?all=1&access_token=fae0e8d5d55b71afb4c59d6abb89fce457c48160',
+        `https://api.github.com/notifications?all=1&access_token=fae0e8d5d55b71afb4c59d6abb89fce457c48160&timestamp=${Date.now()}`,
       )
       const notifications = await response.json()
-      if (Array.isArray(notifications)) this.setState({ notifications })
+      if (Array.isArray(notifications)) {
+        this.setState({
+          notifications: _.orderBy(notifications, ['updated_at'], ['desc']),
+        })
+      }
     } catch (error) {
       console.error(error)
       Alert.alert('Failed to load notifications', `${error}`)
