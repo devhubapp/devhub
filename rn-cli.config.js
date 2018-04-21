b44746586dc27e237c2be99ac94513f6a4767ed8
Bruno Lemos <brunohplemos@gmail.com>
2018-04-21 03:30:53 -0300

    Blacklist .history folder

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -12,6 +12,9 @@ module.exports = {
   // Workaround for RN 0.52 issue with react-native-vector-icons
   // See: https://github.com/oblador/react-native-vector-icons/issues/626#issuecomment-357469857
   getBlacklistRE() {
-    return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/])
+    return blacklist([
+      /\.history.*/,
+      /react-native\/local-cli\/core\/__fixtures__.*/,
+    ])
   },
 }
