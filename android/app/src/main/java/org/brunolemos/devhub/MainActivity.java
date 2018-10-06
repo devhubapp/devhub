2bc6ad8e0da1e50cdd137886d56c2de80b06a6ab
Bruno Lemos <brunohplemos@gmail.com>
2018-10-05 21:08:41 -0300

    [Android] Regenerate project

--- android/app/src/main/java/org/brunolemos/devhub/MainActivity.java
+++ android/app/src/main/java/org/brunolemos/devhub/MainActivity.java
@@ -1,28 +1,15 @@
 package org.brunolemos.devhub;
 
-import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, true);
-        super.onCreate(savedInstanceState);
-    }
 
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
     @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-        };
+    protected String getMainComponentName() {
+        return "devhub";
     }
 }
--- android/app/src/main/java/org/brunolemos/devhub/MainApplication.java
+++ android/app/src/main/java/org/brunolemos/devhub/MainApplication.java
@@ -2,49 +2,44 @@ package org.brunolemos.devhub;
 
 import android.app.Application;
 
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.facebook.react.BuildConfig;
 import com.facebook.react.ReactApplication;
-import com.swmansion.rnscreens.RNScreenPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.asList(
-                    new MainReactPackage(),
-                    new RNScreenPackage(),
-                    new LinearGradientPackage(),
-                    new RNGestureHandlerPackage(),
-                    new SplashScreenReactPackage(),
-                    new VectorIconsPackage()
-            );
-        }
-
-        @Override
-        protected String getJSMainModuleName() {
-            return "index";
-        }
-    };
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
     @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage()
+      );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
