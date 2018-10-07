e15a5830dc3c5daa08611fefb27af1c7bda56779
Bruno Lemos <brunohplemos@gmail.com>
2018-10-07 03:39:15 -0300

    Remove whatwg-url

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 import { AppRegistry } from 'react-native'
-import { URL, URLSearchParams } from 'whatwg-url'
 import { useScreens } from 'react-native-screens'
 
 import App from './src/components/App.tsx'
 
 useScreens()
-
-// see https://github.com/facebook/react-native/issues/16434
-global.URL = URL
-global.URLSearchParams = URLSearchParams
 
 AppRegistry.registerComponent('devhub', () => App)
--- index.web.js
+++ index.web.js
@@ -1,14 +1,9 @@
 import { AppRegistry } from 'react-native'
-import { URL, URLSearchParams } from 'whatwg-url'
 
 import './web/src/index.css'
 import './web/src/reset.css'
 
 import App from './src/components/App.tsx'
-
-// see https://github.com/facebook/react-native/issues/16434
-global.URL = URL
-global.URLSearchParams = URLSearchParams
 
 AppRegistry.registerComponent('devhub', () => App)
 AppRegistry.runApplication('devhub', {
