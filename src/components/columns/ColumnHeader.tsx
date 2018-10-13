8d8d0993bfa0420c09404bcd9ed8b3262a06d0bc
Bruno Lemos <brunohplemos@gmail.com>
2018-10-13 19:34:18 -0300

    Use a different color on column header

--- src/components/columns/ColumnHeader.tsx
+++ src/components/columns/ColumnHeader.tsx
@@ -1,8 +1,13 @@
 import React, { PureComponent, ReactNode } from 'react'
 import { StyleProp, StyleSheet, View, ViewProps, ViewStyle } from 'react-native'
-import { contentPadding } from '../../styles/variables'
 
-export interface IProps extends ViewProps {
+import { contentPadding } from '../../styles/variables'
+import { ThemeConsumer } from '../context/ThemeContext'
+import { columnHeaderItemContentSize } from './ColumnHeaderItem'
+
+export const columnHeaderHeight = 64
+
+export interface ColumnHeaderProps extends ViewProps {
   children?: ReactNode
   maxWidth?: number
   style?: StyleProp<ViewStyle>
@@ -12,21 +17,32 @@ const styles = StyleSheet.create({
   container: {
     alignSelf: 'stretch',
     flexDirection: 'row',
-    minHeight: 48,
+    height: contentPadding * 2 + columnHeaderItemContentSize,
     paddingVertical: contentPadding,
-  } as ViewStyle,
+  },
 
-  buttonContainer: {} as ViewStyle,
+  buttonContainer: {},
 })
 
-export default class ColumnHeader extends PureComponent<IProps> {
+export default class ColumnHeader extends PureComponent<ColumnHeaderProps> {
   render() {
     const { children, style, ...props } = this.props
 
     return (
-      <View {...props} style={[styles.container, style]}>
-        {children}
-      </View>
+      <ThemeConsumer>
+        {({ theme }) => (
+          <View
+            {...props}
+            style={[
+              styles.container,
+              { backgroundColor: theme.backgroundColorLess08 },
+              style,
+            ]}
+          >
+            {children}
+          </View>
+        )}
+      </ThemeConsumer>
     )
   }
 }
