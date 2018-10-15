73738838bd39960603593b6894577d6255d3c292
Bruno Lemos <brunohplemos@gmail.com>
2018-10-15 08:32:57 -0300

    Small fix on dark-gray border colors

--- src/styles/themes/custom.ts
+++ src/styles/themes/custom.ts
@@ -10,16 +10,16 @@ export function createThemeFromColor(
   const isDark = luminance <= 0.5
 
   const backgroundColor = color
-  const backgroundColorMore08 =
-    luminance >= 0.8
-      ? luminance >= 0.95
-        ? darken(0.08, color)
-        : lighten(0.08, color)
-      : luminance <= 0.05
-        ? lighten(0.08, color)
-        : darken(0.08, color)
-  const backgroundColorLess08 =
-    luminance >= 0.8 ? darken(0.08, color) : lighten(0.08, color)
+  const backgroundColorMore08 = isDark
+    ? luminance <= 0.02
+      ? lighten(0.08, color)
+      : darken(0.08, color)
+    : luminance >= 0.95
+      ? darken(0.08, color)
+      : lighten(0.08, color)
+  const backgroundColorLess08 = isDark
+    ? lighten(0.08, color)
+    : darken(0.08, color)
   const backgroundColorTransparent10 = rgba(backgroundColor, 0.1)
   const foregroundColor = isDark ? lighten(0.8, color) : darken(0.8, color)
   const foregroundColorMuted50 = isDark
