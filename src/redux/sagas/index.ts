8e56b69441acb827fa445dbd97368a5fc449cea4
Bruno Lemos <brunohplemos@gmail.com>
2018-10-20 13:22:59 -0300

    [Redux] Setup redux-saga

---
+++ src/redux/sagas/index.ts
@@ -0,0 +1,5 @@
+import { all } from 'redux-saga/effects'
+
+export function* rootSaga() {
+  yield all([])
+}
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,25 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import createSagaMiddleware from 'redux-saga'
 
 import { rootReducer } from './reducers'
+import { rootSaga } from './sagas'
 
 export function configureStore(key = 'root') {
   const persistConfig = { key, storage }
   const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-  const store = createStore(persistedReducer, composeWithDevTools())
+  const sagaMiddleware = createSagaMiddleware()
+
+  const store = createStore(
+    persistedReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware)),
+  )
   const persistor = persistStore(store)
+
+  sagaMiddleware.run(rootSaga)
 
   return { store, persistor }
 }
