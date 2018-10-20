6f603be6b3779af65a33fa9d87f2fc1bb56d6216
Bruno Lemos <brunohplemos@gmail.com>
2018-10-20 13:15:03 -0300

    [Redux] Setup redux-persist

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,39 @@
 import React, { PureComponent } from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { AppNavigator } from '../navigation/AppNavigator'
-import { store } from '../redux/store'
+import { configureStore } from '../redux/store'
 import { AppGlobalStyles } from './AppGlobalStyles'
 import { ColumnsProvider } from './context/ColumnsContext'
 import { DimensionsProvider } from './context/DimensionsContext'
 import { ThemeProvider } from './context/ThemeContext'
 import { UserConsumer, UserProvider } from './context/UserContext'
 
+const { persistor, store } = configureStore()
+
 export class App extends PureComponent {
   render() {
     return (
       <ReduxProvider store={store}>
-        <DimensionsProvider>
-          <UserProvider>
-            <ThemeProvider>
-              <UserConsumer>
-                {({ user }) => (
-                  <ColumnsProvider username={(user && user.login) || null}>
-                    <>
-                      <AppGlobalStyles />
-                      <AppNavigator />
-                    </>
-                  </ColumnsProvider>
-                )}
-              </UserConsumer>
-            </ThemeProvider>
-          </UserProvider>
-        </DimensionsProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <DimensionsProvider>
+            <UserProvider>
+              <ThemeProvider>
+                <UserConsumer>
+                  {({ user }) => (
+                    <ColumnsProvider username={(user && user.login) || null}>
+                      <>
+                        <AppGlobalStyles />
+                        <AppNavigator />
+                      </>
+                    </ColumnsProvider>
+                  )}
+                </UserConsumer>
+              </ThemeProvider>
+            </UserProvider>
+          </DimensionsProvider>
+        </PersistGate>
       </ReduxProvider>
     )
   }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,16 @@
 import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from './reducers'
 
-export const store = createStore(rootReducer, composeWithDevTools())
+export function configureStore(key = 'root') {
+  const persistConfig = { key, storage }
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+  const store = createStore(persistedReducer, composeWithDevTools())
+  const persistor = persistStore(store)
+
+  return { store, persistor }
+}
