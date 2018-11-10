b0f0fa77ede97af5180739297f37978b90143b3b
Bruno Lemos <brunohplemos@gmail.com>
2018-11-10 05:38:18 -0200

    Disable StrictMode again while react-native doesnt support it
    https://github.com/facebook/react-native/issues/22186

--- packages/shared/src/components/App.tsx
+++ packages/shared/src/components/App.tsx
@@ -10,27 +10,25 @@ import { ThemeProvider } from './context/ThemeContext'
 
 const { persistor, store } = configureStore()
 
-// TODO: Enable StrictMode after react-redux fixes it
-// @see https://github.com/reduxjs/react-redux/issues/897
-// @see https://github.com/reduxjs/react-redux/issues/890
-// @see https://github.com/reduxjs/react-redux/issues/950
+// TODO: Enable StrictMode after react-native fixes it
+// @see https://github.com/facebook/react-native/issues/22186
 export class App extends PureComponent {
   render() {
     return (
-      <StrictMode>
-        <ReduxProvider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <DimensionsProvider>
-              <ThemeProvider>
-                <>
-                  <AppGlobalStyles />
-                  <AppNavigator />
-                </>
-              </ThemeProvider>
-            </DimensionsProvider>
-          </PersistGate>
-        </ReduxProvider>
-      </StrictMode>
+      // <StrictMode>
+      <ReduxProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <DimensionsProvider>
+            <ThemeProvider>
+              <>
+                <AppGlobalStyles />
+                <AppNavigator />
+              </>
+            </ThemeProvider>
+          </DimensionsProvider>
+        </PersistGate>
+      </ReduxProvider>
+      // </StrictMode>
     )
   }
 }
