5f300c4deb4c7df44220e39bb6e665812dbf775d
Bruno Lemos <brunohplemos@gmail.com>
2018-11-21 15:46:14 -0200

    Fixed www redirect using 301 status code
    Related: https://github.com/zeit/now-cli/issues/1670

--- packages/server-www/index.js
+++ packages/server-www/index.js
@@ -1,11 +1,6 @@
 module.exports = (req, res) => {
-  const url = `https://devhubapp.com/${req.url.substr(1)}`
+  res.statusCode = 301
+  res.setHeader('Location', `https://devhubapp.com/${req.url.substr(1)}`)
 
-  res.end(
-    `<!DOCTYPE html>
-    <meta charset="utf-8" />
-    <meta http-equiv="refresh" content=${JSON.stringify(`0;URL=${url}`)} />
-    <title>Redirecting...</title>
-    <script>window.location=${JSON.stringify(url)}</script>`,
-  )
+  res.end()
 }
