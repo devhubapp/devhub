69b65fd59d6e6e7f18d397178e1456bd6abe12ec
Bruno Lemos <brunohplemos@gmail.com>
2018-12-11 02:58:28 -0200

    Remove react-native-google-analytics-bridge

--- packages/components/src/libs/analytics/index.native.ts
+++ packages/components/src/libs/analytics/index.native.ts
@@ -1,18 +1,6 @@
 import * as firebase from 'react-native-firebase'
-import {
-  GoogleAnalyticsSettings,
-  GoogleAnalyticsTracker,
-} from 'react-native-google-analytics-bridge'
 
-import { appVersion } from '../../components/common/AppVersion'
 import { Analytics } from './'
-
-GoogleAnalyticsSettings.setDryRun(false)
-GoogleAnalyticsSettings.setDispatchInterval(5)
-
-const tracker = new GoogleAnalyticsTracker('UA-52350759-2')
-tracker.setAppName('devhub-app')
-tracker.setAppVersion(appVersion)
 
 const log = (...args: any[]) => {
   console.log('[ANALYTICS]', ...args) // tslint:disable-line no-console
@@ -21,13 +9,11 @@ const log = (...args: any[]) => {
 export const analytics: Analytics = {
   setUser(userId) {
     if (__DEV__) log('set', { user_id: userId })
-    tracker.setUser(userId)
     firebase.analytics().setUserId(userId)
   },
 
   trackEvent(category, action) {
     if (__DEV__) log('event', category, action)
-    tracker.trackEvent(category, action)
     firebase.analytics().logEvent(action, { category })
   },
 
@@ -37,7 +23,6 @@ export const analytics: Analytics = {
 
   trackScreenView(screenName) {
     if (__DEV__) log('screen_view', screenName)
-    tracker.trackScreenView(screenName)
     firebase.analytics().setCurrentScreen(screenName)
   },
 }
