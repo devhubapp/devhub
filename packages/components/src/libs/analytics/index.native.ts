165dfc112c50287883beff6117006f649d7ea6d0
Bruno Lemos <brunohplemos@gmail.com>
2018-12-11 03:05:39 -0200

    Revert "Remove react-native-google-analytics-bridge"
    This reverts commit d509b5b9243b0d6acc919549cae17d17301ccb86.

--- packages/components/src/libs/analytics/index.native.ts
+++ packages/components/src/libs/analytics/index.native.ts
@@ -1,6 +1,18 @@
 import * as firebase from 'react-native-firebase'
+import {
+  GoogleAnalyticsSettings,
+  GoogleAnalyticsTracker,
+} from 'react-native-google-analytics-bridge'
 
+import { appVersion } from '../../components/common/AppVersion'
 import { Analytics } from './'
+
+GoogleAnalyticsSettings.setDryRun(false)
+GoogleAnalyticsSettings.setDispatchInterval(5)
+
+const tracker = new GoogleAnalyticsTracker('UA-52350759-2')
+tracker.setAppName('devhub-app')
+tracker.setAppVersion(appVersion)
 
 const log = (...args: any[]) => {
   console.log('[ANALYTICS]', ...args) // tslint:disable-line no-console
@@ -9,11 +21,13 @@ const log = (...args: any[]) => {
 export const analytics: Analytics = {
   setUser(userId) {
     if (__DEV__) log('set', { user_id: userId })
+    tracker.setUser(userId)
     firebase.analytics().setUserId(userId)
   },
 
   trackEvent(category, action) {
     if (__DEV__) log('event', category, action)
+    tracker.trackEvent(category, action)
     firebase.analytics().logEvent(action, { category })
   },
 
@@ -23,6 +37,7 @@ export const analytics: Analytics = {
 
   trackScreenView(screenName) {
     if (__DEV__) log('screen_view', screenName)
+    tracker.trackScreenView(screenName)
     firebase.analytics().setCurrentScreen(screenName)
   },
 }
