b1f2ecac21f019b87b54a1fc438ceaa34d92136e
Bruno Lemos <brunohplemos@gmail.com>
2018-12-19 02:37:55 -0200

    Fix Splash Screen

--- packages/mobile/ios/devhub/AppDelegate.m
+++ packages/mobile/ios/devhub/AppDelegate.m
@@ -11,8 +11,7 @@
 #import <React/RCTLinkingManager.h>
 #import <React/RCTRootView.h>
 
-//#import "RNSplashScreen.h"
-
+#import "RNSplashScreen.h"
 #import <Firebase.h>
 
 @implementation AppDelegate
@@ -20,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-  
+
   NSString *ENTRY_FILE = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"ENTRY_FILE"];
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:ENTRY_FILE fallbackResource:nil];
   
@@ -28,18 +27,18 @@
                                                       moduleName:@"devhub"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-  
+  rootView.backgroundColor = [UIColor colorWithRed:0.16 green:0.17 blue:0.20 alpha:1.0];
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
-//  [RNSplashScreen show];
-  
+
   [FIRApp configure];
-  
+
+  [RNSplashScreen show];
+
   return YES;
 }
 
