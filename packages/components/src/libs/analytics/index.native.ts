16cdfde40507391b991ced6202c315f405585a9c
Bruno Lemos <brunohplemos@gmail.com>
2019-01-07 22:58:40 -0200

    [Analytics] Set is_electron custom dimension

--- packages/components/src/libs/analytics/index.native.ts
+++ packages/components/src/libs/analytics/index.native.ts
@@ -17,34 +17,36 @@ tracker.setAppName(__DEV__ ? 'devhub-dev' : 'devhub')
 tracker.setAppVersion(appVersion)
 tracker.customDimensionsFieldsIndexMap = {
   user_id: 1,
+  is_electron: 2,
 }
 
 const log = (...args: any[]) => {
   // console.log('[ANALYTICS]', ...args) // tslint:disable-line no-console
 }
 
+let _userId: string
 export const analytics: Analytics = {
   setUser(userId) {
+    _userId = userId
+
     if (__DEV__) log('set', { user_id: userId })
     tracker.setUser(userId)
     firebase.analytics().setUserId(userId)
   },
 
   trackEvent(category, action, label, value, payload = {}) {
+    // TODO: Test this and fix
+    const customDimensions = _.isPlainObject(payload)
+      ? payload
+      : typeof payload === 'string' || typeof payload === 'number'
+      ? ({ payload } as any)
+      : {}
+
+    customDimensions.user_id = _userId
+    customDimensions.is_electron = false
+
     if (__DEV__) log('event', category, action)
-    tracker.trackEvent(
-      category,
-      action,
-      { label, value },
-      {
-        // TODO: Test this and fix
-        customDimensions: _.isPlainObject(payload)
-          ? payload
-          : typeof payload === 'string' || typeof payload === 'number'
-          ? ({ payload } as any)
-          : undefined,
-      },
-    )
+    tracker.trackEvent(category, action, { label, value }, { customDimensions })
     firebase.analytics().logEvent(action.replace(/\//g, '_'), {
       event_category: category,
       event_label: label,
@@ -59,7 +61,9 @@ export const analytics: Analytics = {
 
   trackScreenView(screenName) {
     if (__DEV__) log('screen_view', screenName)
-    tracker.trackScreenView(screenName)
+    tracker.trackScreenView(screenName, {
+      customDimensions: { user_id: _userId, is_electron: false },
+    })
     firebase.analytics().setCurrentScreen(screenName)
   },
 }
