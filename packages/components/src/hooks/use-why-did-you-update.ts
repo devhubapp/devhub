686d733dd2a67083732b6e6b17c1d222b06cbd20
Bruno Lemos <brunohplemos@gmail.com>
2019-01-17 04:26:13 -0200

    [Dev] New hook: useWhyDidYouUpdate

---
+++ packages/components/src/hooks/use-why-did-you-update.ts
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react'
+
+export function useWhyDidYouUpdate(name: string, props: Record<string, any>) {
+  const latestProps = useRef(props)
+
+  useEffect(() => {
+    if (!__DEV__) return
+
+    const allKeys = Object.keys({ ...latestProps.current, ...props })
+
+    const changesObj: Record<string, { from: any; to: any }> = {}
+    allKeys.forEach(key => {
+      if (latestProps.current[key] !== props[key]) {
+        changesObj[key] = { from: latestProps.current[key], to: props[key] }
+      }
+    })
+
+    if (Object.keys(changesObj).length) {
+      // tslint:disable-next-line no-console
+      console.log('[xxx][why-did-update-you-update]', name, changesObj)
+    }
+
+    latestProps.current = props
+  }, Object.values(props))
+}
