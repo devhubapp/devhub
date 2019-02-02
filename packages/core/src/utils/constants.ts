d1b2d49cc77f8a2438068a95c346443b9d33ef60
Bruno Lemos <brunohplemos@gmail.com>
2019-02-02 18:24:49 -0200

    Change default themes to light-blue and dark-blue

--- packages/core/src/utils/constants.ts
+++ packages/core/src/utils/constants.ts
@@ -1,8 +1,8 @@
 import { ThemePair } from '../types'
 
 export const COLUMNS_LIMIT = 20
-export const DEFAULT_DARK_THEME = 'dark-gray'
-export const DEFAULT_LIGHT_THEME = 'light-gray'
+export const DEFAULT_DARK_THEME = 'light-blue'
+export const DEFAULT_LIGHT_THEME = 'dark-blue'
 export const DEFAULT_PAGINATION_PER_PAGE = 10
 export const DEFAULT_THEME_PAIR: ThemePair = { id: 'auto', color: '' }
 export const MAX_COLUMN_WIDTH = 360
