0ab234ab0fcb3981e6b5b324505ccd6bef9131af
renatoagds <renato_0603@hotmail.com>
2019-02-10 17:52:28 -0200

    feat: make sidebar items bigger

--- packages/components/src/styles/variables.ts
+++ packages/components/src/styles/variables.ts
@@ -6,7 +6,8 @@ export const smallAvatarSize = avatarSize / 2
 export const smallTextSize = 12
 
 export const columnHeaderItemContentSize = 20
-export const columnHeaderItemContentBiggerSize = columnHeaderItemContentSize + 2
+export const columnHeaderItemContentBiggerSize =
+  columnHeaderItemContentSize + 10
 export const columnHeaderHeight =
   contentPadding * 2 + columnHeaderItemContentSize
 export const sidebarSize =
