dd68db62a18daa4175e44d9035e5081df2577474
Juliano Rafael <julianorsfarias@msn.com>
2019-02-18 17:35:51 -0300

    Refactor useKeyboardScrolling to improve readability

--- packages/components/src/hooks/use-keyboard-scrolling.ts
+++ packages/components/src/hooks/use-keyboard-scrolling.ts
@@ -13,38 +13,38 @@ export function useKeyboardScrolling({
   columnId,
   length,
 }: KeyboardScrollingConfig) {
-  const [selectedColumnIndex, setSelectedColumnIndex] = React.useState(0)
+  const [selectedCardIndex, setSelectedCardIndex] = React.useState(0)
   useEmitter(
     'SCROLL_DOWN_COLUMN',
     (payload: { columnId: string }) => {
       if (!ref.current) return
       if (columnId !== payload.columnId) return
-      const index = selectedColumnIndex + 1
+      const index = selectedCardIndex + 1
       if (index < length) {
         ref.current.scrollToIndex({
           animated: true,
           index,
         })
-        setSelectedColumnIndex(index)
+        setSelectedCardIndex(index)
       }
     },
-    [selectedColumnIndex, length],
+    [selectedCardIndex, length],
   )
   useEmitter(
     'SCROLL_UP_COLUMN',
     (payload: { columnId: string }) => {
       if (!ref.current) return
       if (columnId !== payload.columnId) return
-      const index = selectedColumnIndex - 1
+      const index = selectedCardIndex - 1
       if (index >= 0) {
         ref.current.scrollToIndex({
           animated: true,
           index,
         })
-        setSelectedColumnIndex(index)
+        setSelectedCardIndex(index)
       }
     },
-    [selectedColumnIndex, length],
+    [selectedCardIndex, length],
   )
-  return selectedColumnIndex
+  return selectedCardIndex
 }
