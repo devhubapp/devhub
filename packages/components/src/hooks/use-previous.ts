c185cba201080bbbe58edad54604239e1459c0cf
Bruno Lemos <brunohplemos@gmail.com>
2019-02-20 07:03:56 -0300

    Fix usePrevious type definition

--- packages/components/src/hooks/use-previous.ts
+++ packages/components/src/hooks/use-previous.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 
-export function usePrevious(value: any) {
-  const ref = useRef(undefined)
+export function usePrevious<T>(value: T) {
+  const ref = useRef<T | undefined>(undefined)
 
   useEffect(
     () => {
