9d5d9ea5b88a427d4ac1eccb705af6a41df27b04
Bruno Lemos <brunohplemos@gmail.com>
2019-02-23 21:33:58 -0300

    Link app version to GitHub Releases

--- packages/components/src/components/common/AppVersion.tsx
+++ packages/components/src/components/common/AppVersion.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native'
 
 import { useCSSVariablesOrSpringAnimatedTheme } from '../../hooks/use-css-variables-or-spring--animated-theme'
 import { SpringAnimatedText } from '../animated/spring/SpringAnimatedText'
+import { Link } from './Link'
 
 const pkg = require('@devhub/core/package.json') // tslint:disable-line
 
@@ -21,11 +22,17 @@ export function AppVersion() {
   const springAnimatedTheme = useCSSVariablesOrSpringAnimatedTheme()
 
   return (
-    <SpringAnimatedText
-      style={[
-        styles.appVersion,
-        { color: springAnimatedTheme.foregroundColorMuted50 },
-      ]}
-    >{`v${appVersion}`}</SpringAnimatedText>
+    <Link
+      analyticsLabel="app_version"
+      href="https://github.com/devhubapp/devhub/releases"
+      openOnNewTab
+    >
+      <SpringAnimatedText
+        style={[
+          styles.appVersion,
+          { color: springAnimatedTheme.foregroundColorMuted50 },
+        ]}
+      >{`v${appVersion}`}</SpringAnimatedText>
+    </Link>
   )
 }
