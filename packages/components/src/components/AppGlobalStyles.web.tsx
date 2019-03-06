b8ff50dfb77f8d699980e12cc47e5c23365c98da
Bruno Lemos <brunohplemos@gmail.com>
2019-03-06 16:51:36 -0300

    [Web] Re-add custom scrollbar css
    Because the default one is too ugly on linux/windows, specially using a dark theme

--- packages/components/src/components/AppGlobalStyles.web.tsx
+++ packages/components/src/components/AppGlobalStyles.web.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 
 import { Theme } from '@devhub/core'
-import { darken, lighten } from 'polished'
+import { darken, getLuminance, lighten } from 'polished'
 import { themeColorFields } from '../utils/helpers/theme'
+import { getSeparatorThemeColor } from './common/Separator'
 import { useTheme } from './context/ThemeContext'
 
 function getStyles(params: { theme: Theme }) {
   const t = params.theme
+  const separatorColor = t[getSeparatorThemeColor(t.backgroundColor)]
+  const separatorColorLuminance = getLuminance(separatorColor)
+  const backgroundColorLuminance = getLuminance(t.backgroundColor)
 
   return `
+    ::-webkit-scrollbar-thumb {
+      background-color: ${
+        separatorColorLuminance > backgroundColorLuminance
+          ? lighten(0.1, separatorColor)
+          : darken(0.1, separatorColor)
+      };
+    }
+
     *:focus {
       outline-color: ${t.primaryBackgroundColor};
     }
