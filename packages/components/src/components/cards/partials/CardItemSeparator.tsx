2eb0ddfb35f760e03e2887ba1a656ee765f4e401
Bruno Lemos <brunohplemos@gmail.com>
2019-03-20 23:26:08 -0300

    Fix separator color on read items

--- packages/components/src/components/cards/partials/CardItemSeparator.tsx
+++ packages/components/src/components/cards/partials/CardItemSeparator.tsx
@@ -1,26 +1,33 @@
 import { getLuminance } from 'polished'
 import React from 'react'
 
-import { ThemeColors } from '@devhub/core'
+import { isItemRead, ThemeColors } from '@devhub/core'
 import { Separator } from '../../common/Separator'
 import { useTheme } from '../../context/ThemeContext'
 
 export function getCardItemSeparatorThemeColor(
   backgroundColor: string,
+  isRead?: boolean,
 ): keyof ThemeColors {
   const luminance = getLuminance(backgroundColor)
 
   if (luminance <= 0.02) return 'backgroundColorLess2'
-  return 'backgroundColorDarker1'
+
+  return isRead ? 'backgroundColorDarker2' : 'backgroundColorDarker1'
 }
 
-export function CardItemSeparator() {
+export function CardItemSeparator(props: any) {
+  const { leadingItem } = props
+
   const theme = useTheme()
+
+  const isRead = leadingItem ? isItemRead(leadingItem) : false
 
   return (
     <Separator
       backgroundThemeColor={getCardItemSeparatorThemeColor(
         theme.backgroundColor,
+        isRead,
       )}
       horizontal
     />
