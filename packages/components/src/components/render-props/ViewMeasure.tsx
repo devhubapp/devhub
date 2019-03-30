4e632a715cb24b989b1d66d004b2f72e8afc9716
Bruno Lemos <brunohplemos@gmail.com>
2019-03-30 00:17:24 -0300

    [Performance] Avoid some ViewMeasure unnecessary re-renders
    https://github.com/devhubapp/devhub/issues/126

--- packages/components/src/components/render-props/ViewMeasure.tsx
+++ packages/components/src/components/render-props/ViewMeasure.tsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
-import { LayoutRectangle, View, ViewProps } from 'react-native'
+import { View, ViewProps } from 'react-native'
 
 import { Omit } from '@devhub/core'
 
 export interface ViewMeasurerProps extends Omit<ViewProps, 'children'> {
-  children: (layout: LayoutRectangle) => React.ReactNode
+  children: (layout: { width: number; height: number }) => React.ReactNode
 }
 
 export function ViewMeasurer(props: ViewMeasurerProps) {
   const { children, onLayout, ...otherProps } = props
-  const [layout, setLayout] = useState<LayoutRectangle | null>(null)
+  const [layout, setLayout] = useState<{
+    width: number
+    height: number
+  } | null>(null)
 
   return (
     <View
       {...otherProps}
       onLayout={e => {
+        const { width, height } = e.nativeEvent.layout
+        if (layout && width === layout.width && height === layout.height) return
+
         setLayout(e.nativeEvent.layout)
         if (onLayout) onLayout(e)
       }}
