567036d5e02bdb7502f47dfb13492383d6b369ac
Bruno Lemos <brunohplemos@gmail.com>
2019-04-13 23:51:15 -0300

    Add a try catch on findNode method to avoid crashes

--- packages/components/src/utils/helpers/shared.ts
+++ packages/components/src/utils/helpers/shared.ts
@@ -7,18 +7,24 @@ import { Linking } from '../../libs/linking'
 import { Platform } from '../../libs/platform'
 
 export function findNode(ref: any) {
-  let node = ref && (ref.current || ref)
+  try {
+    let node = ref && (ref.current || ref)
 
-  if (node && (node as any).getNode && (node as any).getNode())
-    node = (node as any).getNode()
+    if (node && (node as any).getNode && (node as any).getNode())
+      node = (node as any).getNode()
 
-  if (node && (node as any)._touchableNode) node = (node as any)._touchableNode
+    if (node && (node as any)._touchableNode)
+      node = (node as any)._touchableNode
 
-  if (node && (node as any)._node) node = (node as any)._node
+    if (node && (node as any)._node) node = (node as any)._node
 
-  if (node && Platform.OS === 'web') node = findDOMNode(node)
+    if (node && Platform.OS === 'web') node = findDOMNode(node)
 
-  return node
+    return node
+  } catch (error) {
+    console.error('Failed to find node', error, { ref })
+    return null
+  }
 }
 
 export function tryFocus(ref: any) {
