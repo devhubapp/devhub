e408c1962b2a12650f99d567a05ef3860f89bfea
Bruno Lemos <brunohplemos@gmail.com>
2019-05-10 19:06:01 -0300

    Update slack banner message from existing redux cache

--- packages/components/src/redux/reducers/app.ts
+++ packages/components/src/redux/reducers/app.ts
@@ -1,6 +1,7 @@
 import immer from 'immer'
 
 import { BannerMessage, constants } from '@devhub/core'
+import { REHYDRATE } from 'redux-persist'
 import { Reducer } from '../types'
 
 export interface State {
@@ -23,6 +24,32 @@ const initialState: State = {
 
 export const appReducer: Reducer<State> = (state = initialState, action) => {
   switch (action.type) {
+    case REHYDRATE as any: {
+      const app =
+        action.payload && ((action.payload as any).app as State | undefined)
+
+      return {
+        ...app,
+        banners: ((app && app.banners) || []).map(banner => {
+          if (!(banner && banner.id)) return banner
+
+          const updatedBanner = initialState.banners.find(
+            b => b.id === banner.id,
+          )
+          if (updatedBanner) {
+            return {
+              ...banner,
+              href: updatedBanner.href,
+              message: updatedBanner.message,
+              openOnNewTab: updatedBanner.openOnNewTab,
+            }
+          }
+
+          return banner
+        }),
+      }
+    }
+
     case 'CLOSE_BANNER_MESSAGE':
       return immer(state, draft => {
         draft.banners = draft.banners || []
