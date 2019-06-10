866ddc641b4eabd42523e6c0e4e344427228c55e
Bruno Lemos <brunohplemos@gmail.com>
2019-06-10 00:37:00 -0300

    Possibly fix wrong scroll offset
    Fix https://github.com/devhubapp/devhub/issues/144

--- packages/components/src/components/columns/Columns.tsx
+++ packages/components/src/components/columns/Columns.tsx
@@ -76,7 +76,7 @@ export const Columns = React.memo((props: ColumnsProps) => {
     flatListRef.current.scrollToItem({
       animated: true,
       item: focusedColumnId,
-      viewPosition: 0.5,
+      viewPosition: 0,
     })
   }, [
     flatListRef.current,
@@ -135,7 +135,7 @@ export const Columns = React.memo((props: ColumnsProps) => {
   const flatListStyle = useMemo(
     () => [
       styles.flatlist,
-      sizename > '1-small' && {
+      sizename > '2-medium' && {
         marginHorizontal: -separatorThickSize / 2,
       },
       style,
