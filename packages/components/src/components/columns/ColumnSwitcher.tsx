9ce9baf270b25c03b451c03fa96a55c99f131b72
Bruno Lemos <brunohplemos@gmail.com>
2019-06-20 19:34:36 -0300

    Fix column switcher arrows opacity bug

--- packages/components/src/components/columns/ColumnSwitcher.tsx
+++ packages/components/src/components/columns/ColumnSwitcher.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 import { useAppViewMode } from '../../hooks/use-app-view-mode'
 import { useReduxState } from '../../hooks/use-redux-state'
@@ -7,6 +7,7 @@ import { emitter } from '../../libs/emitter'
 import * as selectors from '../../redux/selectors'
 import { contentPadding } from '../../styles/variables'
 import { fabSize } from '../common/FAB'
+import { Separator } from '../common/Separator'
 import { useColumnFilters } from '../context/ColumnFiltersContext'
 import { useFocusedColumn } from '../context/ColumnFocusContext'
 import { useAppLayout } from '../context/LayoutContext'
@@ -32,31 +33,49 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.2,
     shadowRadius: 6,
-    overflow: 'hidden',
     zIndex: 1000,
+  },
+
+  leftSwitcherContainer: {
+    width: fabSize,
+    height: fabSize,
+    borderRadius: 0,
+    borderTopLeftRadius: fabSize / 2,
+    borderBottomLeftRadius: fabSize / 2,
+    overflow: 'hidden',
   },
 
   leftSwitcher: {
     alignItems: 'center',
     alignContent: 'center',
     justifyContent: 'center',
-    width: fabSize,
-    height: fabSize,
+    width: '100%',
+    height: '100%',
     borderRadius: 0,
     borderTopLeftRadius: fabSize / 2,
     borderBottomLeftRadius: fabSize / 2,
-    opacity: 1,
+    overflow: 'hidden',
+  },
+
+  rightSwitcherContainer: {
+    width: fabSize,
+    height: fabSize,
+    borderRadius: 0,
+    borderTopRightRadius: fabSize / 2,
+    borderBottomRightRadius: fabSize / 2,
+    overflow: 'hidden',
   },
 
   rightSwitcher: {
     alignItems: 'center',
     alignContent: 'center',
     justifyContent: 'center',
-    width: fabSize,
-    height: fabSize,
+    width: '100%',
+    height: '100%',
     borderRadius: 0,
     borderTopRightRadius: fabSize / 2,
     borderBottomRightRadius: fabSize / 2,
+    overflow: 'hidden',
   },
 })
 
@@ -84,56 +103,69 @@ export function ColumnSwitcher() {
 
   return (
     <ThemedView backgroundColor="backgroundColorMore1" style={styles.container}>
-      <ThemedTouchableOpacity
-        analyticsCategory="switch-column-previous"
-        backgroundColor="backgroundColor"
-        hitSlop={{
-          top: contentPadding / 2,
-          bottom: contentPadding / 2,
-          left: contentPadding,
-        }}
-        onPress={() => {
-          emitter.emit('FOCUS_ON_PREVIOUS_COLUMN', {
-            highlight: isFirst,
-          })
-        }}
-        style={[styles.leftSwitcher, !!isFirst && { opacity: 0.5 }]}
-        tooltip={`Previous column (${
-          keyboardShortcutsById.selectPreviousColumn.keys[0]
-        })`}
+      <ThemedView
+        style={[styles.leftSwitcherContainer, { opacity: isFirst ? 0.5 : 1 }]}
       >
-        <ThemedIcon
-          color="foregroundColor"
-          name="chevron-left"
-          size={14}
-          style={{ lineHeight: fabSize / 2.5, fontSize: fabSize / 2.5 }}
-        />
-      </ThemedTouchableOpacity>
-      <ThemedTouchableOpacity
-        analyticsCategory="switch-column-next"
-        backgroundColor="backgroundColor"
-        hitSlop={{
-          top: contentPadding / 2,
-          bottom: contentPadding / 2,
-          right: contentPadding,
-        }}
-        onPress={() => {
-          emitter.emit('FOCUS_ON_NEXT_COLUMN', {
-            highlight: isLast,
-          })
-        }}
-        style={[styles.rightSwitcher, isLast && { opacity: 0.5 }]}
-        tooltip={`Next column (${
-          keyboardShortcutsById.selectNextColumn.keys[0]
-        })`}
+        <ThemedTouchableOpacity
+          activeOpacity={isFirst ? 1 : undefined}
+          analyticsCategory="switch-column-previous"
+          backgroundColor="backgroundColor"
+          hitSlop={{
+            top: contentPadding / 2,
+            bottom: contentPadding / 2,
+            left: contentPadding,
+          }}
+          onPress={() => {
+            emitter.emit('FOCUS_ON_PREVIOUS_COLUMN', {
+              highlight: isFirst,
+            })
+          }}
+          style={styles.leftSwitcher}
+          tooltip={`Previous column (${
+            keyboardShortcutsById.selectPreviousColumn.keys[0]
+          })`}
+        >
+          <ThemedIcon
+            color="foregroundColor"
+            name="chevron-left"
+            size={14}
+            style={{ lineHeight: fabSize / 2.5, fontSize: fabSize / 2.5 }}
+          />
+        </ThemedTouchableOpacity>
+      </ThemedView>
+
+      <Separator horizontal={false} />
+
+      <ThemedView
+        style={[styles.rightSwitcherContainer, { opacity: isLast ? 0.5 : 1 }]}
       >
-        <ThemedIcon
-          color="foregroundColor"
-          name="chevron-right"
-          size={14}
-          style={{ lineHeight: fabSize / 2.5, fontSize: fabSize / 2.5 }}
-        />
-      </ThemedTouchableOpacity>
+        <ThemedTouchableOpacity
+          activeOpacity={isLast ? 1 : undefined}
+          analyticsCategory="switch-column-next"
+          backgroundColor="backgroundColor"
+          hitSlop={{
+            top: contentPadding / 2,
+            bottom: contentPadding / 2,
+            right: contentPadding,
+          }}
+          onPress={() => {
+            emitter.emit('FOCUS_ON_NEXT_COLUMN', {
+              highlight: isLast,
+            })
+          }}
+          style={styles.rightSwitcher}
+          tooltip={`Next column (${
+            keyboardShortcutsById.selectNextColumn.keys[0]
+          })`}
+        >
+          <ThemedIcon
+            color="foregroundColor"
+            name="chevron-right"
+            size={14}
+            style={{ lineHeight: fabSize / 2.5, fontSize: fabSize / 2.5 }}
+          />
+        </ThemedTouchableOpacity>
+      </ThemedView>
     </ThemedView>
   )
 }
