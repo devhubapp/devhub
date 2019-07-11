df31acd3289a43b3d25990c792789460317ba424
Bruno Lemos <brunohplemos@gmail.com>
2019-07-11 17:50:11 -0300

    Move all providers to an AppProviders component

--- packages/components/src/components/App.tsx
+++ packages/components/src/components/App.tsx
@@ -1,58 +1,31 @@
-import React from 'react'
-import { Provider as ReduxProvider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+import React, { Fragment } from 'react'
 
 import '../libs/analytics'
 
-import { HelmetProvider } from '../libs/helmet'
 import { AppNavigator } from '../navigation/AppNavigator'
 import { enableNetworkInterceptors } from '../network-interceptor'
-import { configureStore } from '../redux/store'
 import { AppGlobalStyles } from './AppGlobalStyles'
+import { AppProviders } from './AppProviders'
 import { AppIconBadge } from './common/AppIconBadge'
-import { ColumnFiltersProvider } from './context/ColumnFiltersContext'
-import { ColumnFocusProvider } from './context/ColumnFocusContext'
-import { ColumnWidthProvider } from './context/ColumnWidthContext'
-import { DeepLinkProvider } from './context/DeepLinkContext'
-import { AppLayoutProvider } from './context/LayoutContext'
-import { SpringAnimatedThemeProvider } from './context/SpringAnimatedThemeContext'
 import { UnreadCountProvider } from './context/UnreadCountContext'
 
 enableNetworkInterceptors()
 
-const { persistor, store } = configureStore()
-
 // TODO: Enable StrictMode after react-native fixes it
 // @see https://github.com/facebook/react-native/issues/22186
+const StrictModePlaceholder = Fragment
+
 export function App() {
   return (
-    // <StrictMode>
-    <HelmetProvider>
-      <ReduxProvider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <DeepLinkProvider>
-            <AppLayoutProvider>
-              <ColumnFocusProvider>
-                <ColumnFiltersProvider>
-                  <ColumnWidthProvider>
-                    <SpringAnimatedThemeProvider>
-                      <>
-                        <AppGlobalStyles key="app-global-styles" />
-                        <AppNavigator key="app-navigator" />
+    <StrictModePlaceholder>
+      <AppProviders>
+        <AppGlobalStyles key="app-global-styles" />
+        <AppNavigator key="app-navigator" />
 
-                        <UnreadCountProvider>
-                          <AppIconBadge />
-                        </UnreadCountProvider>
-                      </>
-                    </SpringAnimatedThemeProvider>
-                  </ColumnWidthProvider>
-                </ColumnFiltersProvider>
-              </ColumnFocusProvider>
-            </AppLayoutProvider>
-          </DeepLinkProvider>
-        </PersistGate>
-      </ReduxProvider>
-    </HelmetProvider>
-    // </StrictMode>
+        <UnreadCountProvider>
+          <AppIconBadge />
+        </UnreadCountProvider>
+      </AppProviders>
+    </StrictModePlaceholder>
   )
 }
---
+++ packages/components/src/components/AppProviders.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Provider as ReduxProvider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { HelmetProvider } from '../libs/helmet'
+import { configureStore } from '../redux/store'
+import { ColumnFiltersProvider } from './context/ColumnFiltersContext'
+import { ColumnFocusProvider } from './context/ColumnFocusContext'
+import { ColumnWidthProvider } from './context/ColumnWidthContext'
+import { DeepLinkProvider } from './context/DeepLinkContext'
+import { AppLayoutProvider } from './context/LayoutContext'
+import { SpringAnimatedThemeProvider } from './context/SpringAnimatedThemeContext'
+
+const { persistor, store } = configureStore()
+
+export interface AppProvidersProps {
+  children: React.ReactNode
+}
+
+export function AppProviders(props: AppProvidersProps) {
+  return (
+    <HelmetProvider>
+      <ReduxProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <DeepLinkProvider>
+            <AppLayoutProvider>
+              <ColumnFocusProvider>
+                <ColumnFiltersProvider>
+                  <ColumnWidthProvider>
+                    <SpringAnimatedThemeProvider>
+                      {props.children}
+                    </SpringAnimatedThemeProvider>
+                  </ColumnWidthProvider>
+                </ColumnFiltersProvider>
+              </ColumnFocusProvider>
+            </AppLayoutProvider>
+          </DeepLinkProvider>
+        </PersistGate>
+      </ReduxProvider>
+    </HelmetProvider>
+  )
+}
