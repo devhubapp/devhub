4d783aeb1ffe18a1bff256f0b6bd75adf9f9e853
Bruno Lemos <brunohplemos@gmail.com>
2019-07-31 00:30:56 -0300

    [Dev] Improve error message when a redux action causes a crash

--- packages/components/src/redux/middlewares/bugsnag.ts
+++ packages/components/src/redux/middlewares/bugsnag.ts
@@ -9,10 +9,15 @@ export const bugsnagMiddleware: Middleware = () => next => action => {
         action: action.type,
       })
     }
+  } catch (error) {
+    console.error(`Failed to leave bugsnag breadcrumb. ${error}`)
+    throw error
+  }
+
+  try {
     next(action)
   } catch (error) {
-    console.error('Failed to leave bugsnag breadcrumb', error)
-
+    console.error(`Failed to run redux action ${action.type}. ${error}`)
     throw error
   }
 }
