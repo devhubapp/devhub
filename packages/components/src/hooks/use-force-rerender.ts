698da643f414789fa3f86b539064436d7291dfe2
Bruno Lemos <brunohplemos@gmail.com>
2019-08-27 16:46:39 -0300

    Fix forceRerender
    https://twitter.com/alecdotbiz/status/1142506184240521216\?s\=20

--- packages/components/src/hooks/use-force-rerender.ts
+++ packages/components/src/hooks/use-force-rerender.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 export function useForceRerender() {
-  const [, setValue] = useState(false)
+  const [, setValue] = useState(0)
 
-  return () => setValue(value => !value)
+  return () => setValue(value => value + 1)
 }
