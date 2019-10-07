a522f36063b97c88c12893be6dac268f5567d264
Bruno Lemos <brunohplemos@gmail.com>
2019-10-07 05:08:45 -0300

    [Landing] Show "Use web version" button instead of "Other downloads"

--- landing/src/components/sections/download/DownloadButtons.tsx
+++ landing/src/components/sections/download/DownloadButtons.tsx
@@ -5,7 +5,7 @@ import Button from '../../common/buttons/Button'
 export interface DownloadButtonsProps {}
 
 export default function DownloadButtons(_props: DownloadButtonsProps) {
-  const { category, os } = useSystem()
+  const { os } = useSystem()
 
   return (
     <div className="flex flex-row flex-wrap mb-4">
@@ -19,20 +19,14 @@ export default function DownloadButtons(_props: DownloadButtonsProps) {
             {`Download for ${getSystemLabel(os)}`}
           </Button>
 
-          {category === 'desktop' ? (
-            <Button type="neutral" href="/download" className="mb-2">
-              Other downloads
-            </Button>
-          ) : (
-            <Button
-              type="neutral"
-              href="https://app.devhubapp.com/"
-              target="_top"
-              className="mb-2"
-            >
-              Open web version
-            </Button>
-          )}
+          <Button
+            type="neutral"
+            href="https://app.devhubapp.com/"
+            target="_top"
+            className="mb-2"
+          >
+            Use web version
+          </Button>
         </>
       ) : (
         <>
