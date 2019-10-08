2d058ec13fa946f1585fc71aef287af234abdacc
Bruno Lemos <brunohplemos@gmail.com>
2019-10-08 16:35:22 -0300

    [Mobile] Add Intl polyfill, fix crash
    Fix https://github.com/devhubapp/devhub/issues/188

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -2,6 +2,18 @@ import 'react-native-gesture-handler'
 
 import { AppRegistry } from 'react-native'
 
+// Intl polyfill
+import areIntlLocalesSupported from 'intl-locales-supported'
+if (window.Intl) {
+  if (!areIntlLocalesSupported(['en', 'br'])) {
+    var IntlPolyfill = require('intl')
+    window.Intl.NumberFormat = IntlPolyfill.NumberFormat
+    window.Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
+  }
+} else {
+  window.Intl = require('intl')
+}
+
 const { App } = require('@devhub/components/src/components/App')
 
 AppRegistry.registerComponent('devhub', () => App)
