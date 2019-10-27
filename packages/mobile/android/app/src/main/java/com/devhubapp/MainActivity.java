195bd0460420f107c4b98efe1071242d6d9f293f
Bruno Lemos <brunohplemos@gmail.com>
2019-10-27 02:33:11 -0300

    [Android] Increase AsyncStorage limit to 20MB

--- packages/mobile/android/app/src/main/java/com/devhubapp/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/devhubapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.storage.ReactDatabaseSupplier;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import org.devio.rn.splashscreen.SplashScreen;
@@ -13,6 +14,8 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ReactDatabaseSupplier.getInstance(getApplicationContext()).setMaximumSize(20L * 1024L * 1024L); // 20 MB in bytes
+
 //        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
 
         SplashScreen.show(this, R.style.SplashScreenTheme);
