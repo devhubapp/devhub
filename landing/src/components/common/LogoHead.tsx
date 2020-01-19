95103724a87752cc904253d1e6ed91bea9de59f9
Bruno Lemos <brunohplemos@gmail.com>
2020-01-19 12:33:27 -0300

    [Landing] Small fix on logo image quality

--- landing/src/components/common/LogoHead.tsx
+++ landing/src/components/common/LogoHead.tsx
@@ -5,7 +5,7 @@ export function LogoHead() {
     <img
       alt="DevHub logo"
       className="w-20 h-20 m-auto mb-6 bg-primary border-4 border-bg-less-2 rounded-full"
-      src="/static/logo.png"
+      src="/static/logo@2x.png"
     />
   )
 }
