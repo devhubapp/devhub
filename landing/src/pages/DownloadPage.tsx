a41b5a0bddf9afa3aa26fa7e6b0b1ed0a73883c6
Bruno Lemos <brunohplemos@gmail.com>
2020-01-20 04:34:45 -0300

    v0.100.0

--- landing/src/pages/DownloadPage.tsx
+++ landing/src/pages/DownloadPage.tsx
@@ -64,8 +64,8 @@ export default function DownloadPage(_props: DownloadPageProps) {
   const category = _category || system.category || 'web'
   const { os } = system
 
-  const version = '0.99.1'
-  const releaseDate = '2019-12-11'
+  const version = '0.100.0'
+  const releaseDate = '2020-01-20'
 
   return (
     <LandingLayout>
