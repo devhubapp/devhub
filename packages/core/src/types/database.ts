034ae614e6385bb8aac6f5b74cc69c0453c49c2c
Bruno Lemos <brunohplemos@gmail.com>
2020-01-23 20:19:13 -0300

    E-mails field

--- packages/core/src/types/database.ts
+++ packages/core/src/types/database.ts
@@ -65,6 +65,13 @@ export interface DatabaseUserFeedback {
 
 export interface DatabaseUser {
   _id: any
+  emails: Array<{
+    provider: 'github' | 'npm'
+    email: string
+    primary?: boolean
+    verified?: boolean
+    visibility?: 'public' | 'private' | null
+  }>
   columns?: {
     allIds: string[]
     byId: Record<string, Column | undefined>
