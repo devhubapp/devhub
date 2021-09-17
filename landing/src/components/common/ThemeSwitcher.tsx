9d5148ebbe0581e89ebfcddea000902917b76131
KlassiC-Man <parthglt@gmail.com>
2021-09-17 12:15:28 +0530

    Added icons for theme switcher

--- landing/src/components/common/ThemeSwitcher.tsx
+++ landing/src/components/common/ThemeSwitcher.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames'
 import React from 'react'
+import WbSunnyIcon from '@material-ui/icons/WbSunny'
+import Brightness2Icon from '@material-ui/icons/Brightness2'
 
 import { useTheme } from '../../context/ThemeContext'
 
@@ -11,52 +13,56 @@ export function ThemeSwitcher(props: ThemeSwitcherProps) {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <button
-      id="theme-switcher"
-      type="button"
-      className="theme-switcher relative flex-shrink-0 w-10 bg-less-2 rounded-full focus:outline-none"
-      onClick={() => {
-        toggleTheme()
-      }}
-      role="button"
-      aria-label="Toggle theme"
-    >
-      <div
-        className={classNames(
-          'theme-switcher-thumb absolute shadow rounded-full',
-          theme.isDark ? 'dark bg-purple' : 'light bg-yellow',
-        )}
+    <>
+      <WbSunnyIcon style={{ color: 'gray', fontSize: 30, marginRight: 2 }} />
+      <button
+        id="theme-switcher"
+        type="button"
+        className="theme-switcher relative flex-shrink-0 w-10 bg-less-2 rounded-full focus:outline-none ml-10"
+        onClick={() => {
+          toggleTheme()
+        }}
+        role="button"
+        aria-label="Toggle theme"
       >
-        <div className="absolute inset-0 flex items-center justify-center rounded-full bg-white opacity-25" />
-      </div>
+        <div
+          className={classNames(
+            'theme-switcher-thumb absolute shadow rounded-full',
+            theme.isDark ? 'dark bg-purple' : 'light bg-yellow',
+          )}
+        >
+          <div className="absolute inset-0 flex items-center justify-center rounded-full bg-white opacity-25" />
+        </div>
 
-      <style jsx>
-        {`
-          .theme-switcher {
-            height: 1rem;
-            margin-left: 0.2rem;
-            margin-right: 0.2rem;
-          }
+        <style jsx>
+          {`
+            .theme-switcher {
+              height: 1rem;
+              margin-left: 0.2rem;
+              margin-right: 0.2rem;
+            }
 
-          .theme-switcher-thumb {
-            top: -0.2rem;
-            bottom: -0.2rem;
-            width: 1.4rem;
-          }
+            .theme-switcher-thumb {
+              top: -0.2rem;
+              bottom: -0.2rem;
+              width: 1.4rem;
+            }
 
-          .theme-switcher-thumb.light {
-            left: -0.2rem;
-          }
+            .theme-switcher-thumb.light {
+              left: -0.2rem;
+            }
 
-          .theme-switcher-thumb.dark {
-            left: 1.3rem;
-          }
+            .theme-switcher-thumb.dark {
+              left: 1.3rem;
+            }
 
-          .theme-switcher-thumb > div {
-            margin: 2px;
-          }
-        `}
-      </style>
-    </button>
+            .theme-switcher-thumb > div {
+              margin: 2px;
+            }
+          `}
+        </style>
+      </button>
+      <Brightness2Icon style={{ color: 'gray' }} />
+    </>
   )
 }
